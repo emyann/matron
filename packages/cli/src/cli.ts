fb22dd82287d7abaf9568b584c1342f72db6b86f
Yann Renaudin <renaudin.yann@gmail.com>
2018-12-25 21:13:30 +0100

    feat(cli): allow to use add command

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -9,4 +9,6 @@ import { command, parse, option, arguments, action } from 'commander';
 
 version();
 command('create <name>', 'Create a Typescript project');
+command('add <recipe>', 'Runs a recipe against the current project');
+
 parse(process.argv);
