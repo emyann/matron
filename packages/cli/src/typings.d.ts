aa15e96a404fab297a85be3565d8574284b973d8
Yann Renaudin <renaudin.yann@gmail.com>
2019-01-03 03:02:22 -0500

    refactor(cli): create typings file

---
+++ packages/cli/src/typings.d.ts
@@ -0,0 +1,11 @@
+export enum ApplicationType {
+  Typescript = 'typescript'
+}
+export enum Bundler {
+  Webpack = 'webpack',
+  Parcel = 'parcel'
+}
+export enum TestFramework {
+  KarmaJasmine = 'karma-jasmine',
+  Jest = 'jest'
+}
