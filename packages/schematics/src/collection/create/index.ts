c85045301e7abd2683e6e5f54c13298bd8261b50
Yann Renaudin <renaudin.yann@gmail.com>
2019-01-04 10:11:50 -0500

    feat(schematics): no more template usage to bootstrap a project

--- packages/schematics/src/collection/create/index.ts
+++ packages/schematics/src/collection/create/index.ts
@@ -1,21 +1,8 @@
 import { strings, normalize } from '@angular-devkit/core';
-import {
-  Rule,
-  SchematicContext,
-  Tree,
-  mergeWith,
-  move,
-  apply,
-  chain,
-  url,
-  branchAndMerge
-} from '@angular-devkit/schematics';
-import * as spawn from 'cross-spawn';
-import * as path from 'path';
-import {
-  // readJsonInTree,
-  updateJsonInTree
-} from '../../helpers/ast-utils';
+import { Rule, SchematicContext, Tree, chain, schematic } from '@angular-devkit/schematics';
+import path from 'path';
+import { updateJsonInTree } from '../../helpers/ast-utils';
+import { AddSchema } from '../add';
 
 interface Schema {
   name: string;
@@ -24,19 +11,16 @@ interface Schema {
 }
 export function create(options: Schema): Rule {
   return (host: Tree, context: SchematicContext) => {
-    const movePath = normalize('/' + strings.dasherize(options.name));
+    const projectPath = normalize('/' + strings.dasherize(options.name));
 
-    let templateName;
-    if (options.provider !== 'local') {
-      templateName = `@matron/${options.template}`;
-      installTemplate(templateName);
-    } else {
-      templateName = `@matron/templates/${options.template}`;
-    }
-    const templatePath = path.resolve(__dirname, '../../../node_modules', templateName);
-    const template = apply(url(templatePath), [move(movePath)]);
-
-    return chain([branchAndMerge(mergeWith(template)), updatePackageJson(options)])(host, context);
+    const projectName = projectPath.split(path.sep).pop();
+    return chain([
+      schematic<AddSchema>('add', {
+        recipe: 'typescript',
+        projectPath,
+        projectName: projectName ? projectName : strings.dasherize(options.name)
+      })
+    ])(host, context);
   };
 }
 
@@ -48,13 +32,4 @@ export function updatePackageJson(options: Schema): Rule {
       return { scripts, devDependencies, version, main, description };
     });
   };
-}
-
-function installTemplate(templateName: string) {
-  console.log('installing template', templateName);
-
-  const command = 'npm';
-  const args = ['install', templateName, '--loglevel', 'error'];
-
-  spawn.sync(command, args, { stdio: 'inherit', cwd: path.resolve(__dirname) });
 }
