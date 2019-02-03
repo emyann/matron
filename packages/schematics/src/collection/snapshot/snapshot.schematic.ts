c7ac51a40a026bd67935399bcfaadfe3b30f9362
Yann Renaudin <renaudin.yann@gmail.com>
2019-02-02 22:08:31 -0500

    feat: transform path to absolute url when it's not

--- packages/schematics/src/collection/snapshot/snapshot.schematic.ts
+++ packages/schematics/src/collection/snapshot/snapshot.schematic.ts
@@ -10,34 +10,39 @@ export interface SnapshotSchema {
   ignore?: string[];
 }
 
-export function snapshot(options: SnapshotSchema): Rule {
+function getDestinationDirectory(dir: string | undefined) {
   const chance = new Chance();
+
+  if (!dir) {
+    return strings.dasherize(`./${chance.first()}${chance.animal({ type: 'desert' })}`);
+  } else if (!path.isAbsolute(dir)) {
+    return path.join(process.cwd(), dir);
+  } else {
+    return dir;
+  }
+}
+
+export function snapshot(options: SnapshotSchema): Rule {
   const defaultOptions = {
     ignore: ['node_modules', 'node_modules/**/*', 'package-lock.json', 'CHANGELOG.md', '.DS_Store']
   };
   const {
     path: pathToSnapshot = './',
-    destination = strings.dasherize(`./${chance.first()}${chance.animal({ type: 'desert' })}`),
-    ignore
+    ignore = !options.ignore ? defaultOptions.ignore : defaultOptions.ignore.concat(options.ignore)
   } = options;
 
-  const finalOptions = {
-    pathToSnapshot,
-    destination,
-    ignore: !ignore ? defaultOptions.ignore : defaultOptions.ignore.concat(ignore)
-  };
+  const destination = getDestinationDirectory(options.destination);
 
-  console.log('snapshot', pathToSnapshot, destination);
   return (host: Tree, _context: SchematicContext) => {
     const filesPath = globby.sync('**', {
       cwd: pathToSnapshot,
-      ignore: finalOptions.ignore
+      ignore,
+      gitignore: true
     });
     filesPath.forEach(filePath => {
       const targetPath = path.join(destination, filePath);
       const sourcePath = path.join(pathToSnapshot, filePath);
       const fileEntry = host.get(sourcePath);
-      // console.log('filePath', filePath, sourcePath, targetPath);
 
       if (fileEntry) {
         if (!host.exists(targetPath)) {
