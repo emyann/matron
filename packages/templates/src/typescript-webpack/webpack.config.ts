f35ebbc18791956732a5585f571e88df2a719c6d
Yann Renaudin <renaudin.yann@gmail.com>
2019-02-15 18:49:11 -0500

    fix: update template to be publishable

--- packages/templates/src/typescript-webpack/webpack.config.ts
+++ packages/templates/src/typescript-webpack/webpack.config.ts
@@ -8,7 +8,9 @@ const webpackconfiguration: webpack.Configuration = {
   entry: path.resolve(__dirname, 'src'),
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    globalObject: 'this'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json']
